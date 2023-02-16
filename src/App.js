cd96acb5ccacfcb5a1c7acf70ceb682b4fb386ee
SiIverjames <pokesyl@hotmail.com>
2023-02-16 13:33:57 +0100

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import image from "./serpent.jpg";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import Accueil from "./pages/Home";
+import Apropos from "./pages/Apropos";
+import Contact from "./pages/Contact";
+import Portofolio from "./pages/Portofolio";
+import Services from "./pages/Services";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bievenue</h1>
-      <img src={image} alt ="image" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/react-cv' element={<Accueil/>}/>
+        <Route path='/Apropos' element={<Apropos/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
+        <Route path='/Portofolio' element={<Portofolio/>}/>
+        <Route path='/Services' element={<Services/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from "../Menu";
+
+const Services = () => {
+    return (
+        <div>
+            <Menu/>
+            <h1>Services</h1>
+            <p>Donc si vous m'engagez,vous gagnez une personne qui est techinicien Pc et donc qui peut résoudre les problème de pc, un developper Web même s'il y a encore beaucoup de chose à apprendre et un pâtissier qui peut parfois ramener de bonne pâtisserie. </p>
+            <p>Voilà j'ai fini mais je pense que si vous êtes un bon patron ou une bonne patronne il ne vous reste qu'un choix c'est de m'engager alors je vous dis à bientôt!!</p>
+        </div>
+    );
+};
+
+export default Services;
